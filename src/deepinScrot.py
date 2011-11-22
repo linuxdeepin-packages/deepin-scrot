8d1707ea11e94fb2136b97bd8987056b11685515
Andy Stewart <lazycat.manatee@gmail.com>
2011-11-23 00:24:01 +0800

    Change gtk.STOCK_OPEN to gtk.STOCK_SAVE.

--- src/deepinScrot.py
+++ src/deepinScrot.py
@@ -412,7 +412,7 @@ class DeepinScrot:
             None,
             gtk.FILE_CHOOSER_ACTION_SAVE,
             (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-             gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+             gtk.STOCK_SAVE, gtk.RESPONSE_OK))
         dialog.set_default_response(gtk.RESPONSE_OK)
         
         filter = gtk.FileFilter()
