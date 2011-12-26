597d2c64b5a73488e280f6e1001802bb9fbf8114
lovesnow <houshao55@gmail.com>
2011-12-26 16:39:38 +0800

    Update repos.sh for multiple people share.

--- repos.sh
+++ repos.sh
@@ -8,7 +8,7 @@ case "$1" in
         git pull origin master
         ;;
     "push" )
-        git push git@github.com:manateelazycat/deepin-scrot.git
+        git push git@github.com:$2/deepin-scrot.git
         ;;
     "revert" )
         git checkout -- .
