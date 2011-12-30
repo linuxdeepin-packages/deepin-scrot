e30331c144e8088a84cf4c329757b39cbb95c7b1
Zhang Cheng <StephenPCG@gmail.com>
2011-12-31 01:43:08 +0800

    Move updateTranslate.sh outof src/
    User's who install deepin-scrot via apt will not concern locale issuse.
    User's who fetch source directly from git will have to run
    updateTranslate.sh by hand to generate locale/ dir.

--- src/updateTranslate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Update translation.
-
-for poFile in `find ../locale/ -name *.po`
-do
-    base=${poFile%.*}
-    msgfmt $poFile -o $base.mo
-    echo "Update translation: $poFile"
-done    
